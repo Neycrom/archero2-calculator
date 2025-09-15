d1fa251234d612f1ff51a94a52a8d003fc9b9283
Neycrom <zaropha@gmail.com>
2025-09-15 14:01:36 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,21 @@
 const itemBaseValues = {
   gems: 0.0111,
   gold: 0.0000037,
-  scrolls: 0.05
+
+  // Scrolls
+  scroll_weapon: 0.111,
+  scroll_helmet: 0.0377,
+  scroll_armor: 0.0377,
+  scroll_ring: 0.0377,
+  scroll_amulet: 0.0377,
+  scroll_boots: 0.0377,
+  scroll_random: 0.0499,
+
+  // Keys (placeholders for future)
+  key_silver: 0.89,
+  key_obsidian: 3.33,
+  key_chromatic: 4.44,
+
+  // Wish Coin (placeholder)
+  wish_coin: 4.44
 };
